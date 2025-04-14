ad3590714cc9a10ff1839b77b5d16270bcef1ef0
justbord <oli.b-ord@hotmail.co.uk>
2025-04-14 17:51:35 +0100

    Up to Exercise 1.7

--- Part_1/unicafe/src/App.jsx
+++ Part_1/unicafe/src/App.jsx
@@ -1,34 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.rating}</button>
+}
+ 
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  // intially tried to create these values as a function that runs.
+  // but this was not needed as they generate new values with the state change.
+  const all = good+neutral+bad
+
+  //split up the statistics to remove the NaN from showing.
+  if (all!==0) {
+    var average = (good-bad)/all;
+    var positive = (good/all)*100;
+  } else {
+    var average = 0;
+    var positive = 0;
+  }
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Give Feedback</h1>
+        <Button onClick={() => setGood(good+1)} rating="Good" />
+        <Button onClick={() => setNeutral(neutral+1)} rating="Neutral" />
+        <Button onClick={() => setBad(bad+1)} rating="Bad" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <h1>Statistics</h1>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>All: {all} </p>
+        <p>Average: {average} </p>
+        <p>Positive: {positive} %</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
